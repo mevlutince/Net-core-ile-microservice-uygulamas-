7ef721fab2143d9f02dd1e39f7ac46dc684d8635
mevlutince <83275992+mevlutince@users.noreply.github.com>
2021-09-08 19:17:35 +0300

    IdentitiyService created

---
+++ Example.web/Models/SigninInput.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.web.Models
+{
+    public class SigninInput
+    {
+        [Required]
+        [Display(Name ="E-posta adresiniz")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Şİfreniz")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Example.web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,18 @@
+﻿using Example.Shared.Dtos;
+using Example.web.Models;
+using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+
+    }
+}
